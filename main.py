1a33b8d57f7f9d1bbd3a3af5d89757c7ea5d503c
Otavio-Parraga <otavio-parraga@acad.pucrs.br>
2022-03-21 10:25:35 -0300

    Adapted train for bert-like models

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     parser.add_argument('--prefix', action='store_true')
     parser.add_argument('--gpus', type=int, default=1)
     parser.add_argument('-bs', '--batch_size', type=int, default=32)
+    parser.add_argument('-e', '--epochs', type=int, default=10)
 
     args = parser.parse_args()
 
@@ -33,7 +34,7 @@ if __name__ == '__main__':
     print('Loading Model and Tokenizer...')
     pretrained_model, tokenizer = load_model_and_tokenizer(
         args.pretrained_model)
-    model = Code2TestModel(pretrained_model, tokenizer)
+    model = Code2TestModel(args.pretrained_model, pretrained_model, tokenizer)
 
     print('Loading Dataset...')
     train_data = Code2TestDataset(data_dir, 'train', tokenizer, args.prefix)
@@ -57,7 +58,7 @@ if __name__ == '__main__':
 
     print('Training...')
     trainer = pl.Trainer(gpus=args.gpus,
-                         max_epochs=10,
+                         max_epochs=args.epochs,
                          callbacks=[checkpoint_callback,
                                     early_stop_callback],
                          strategy='ddp')
--- model.py
+++ model.py
@@ -1,43 +1,49 @@
 from pathlib import Path
 import torch.optim as optim
 import pytorch_lightning as pl
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, RobertaTokenizer, T5ForConditionalGeneration
+from transformers import AutoTokenizer, RobertaTokenizer, T5ForConditionalGeneration, EncoderDecoderModel, AutoModelForCausalLM, AutoModel
 
 
-def load_model_and_tokenizer(model_name, tokenizer_type='roberta'):
+def load_model_and_tokenizer(model_name):
     cache_path = Path('./pretrained_stuff')
     cache_path.mkdir(exist_ok=True, parents=True)
-    if tokenizer_type == 'roberta':
+    if 'codet5' in model_name.lower():
         return T5ForConditionalGeneration.from_pretrained(model_name, cache_dir='./pretrained_stuff'), \
-               RobertaTokenizer.from_pretrained(model_name, cache_dir='./pretrained_stuff')
+            RobertaTokenizer.from_pretrained(
+                model_name, cache_dir='./pretrained_stuff')
     else:
-        return AutoModelForSeq2SeqLM.from_pretrained(model_name, cache_dir='./pretrained_stuff'), \
-               AutoTokenizer.from_pretrained(model_name, cache_dir='./pretrained_stuff')
+        model = EncoderDecoderModel.from_encoder_decoder_pretrained(model_name, model_name, cache_dir='./pretrained_stuff')
+        tokenizer = RobertaTokenizer.from_pretrained(model_name, cache_dir='./pretrained_stuff')
+        tokenizer = AutoTokenizer.from_pretrained('microsoft/codebert-base')
+        tokenizer.bos_token = tokenizer.cls_token
+        tokenizer.eos_token = tokenizer.sep_token
+        model.config.decoder_start_token_id = tokenizer.bos_token_id
+        model.config.eos_token_id = tokenizer.eos_token_id
+        model.config.pad_token_id = tokenizer.pad_token_id
+        return model, tokenizer
+            
 
 
 class Code2TestModel(pl.LightningModule):
-    def __init__(self, pretrained_model, tokenizer):
+    def __init__(self, pretrained_model_name, pretrained_model, tokenizer):
         super(Code2TestModel, self).__init__()
+        self.pretrained_model_name = pretrained_model_name
         self.pretrained_model = pretrained_model
         self.tokenizer = tokenizer
 
     def configure_optimizers(self):
         optimizer = optim.Adam(self.parameters(), lr=2e-5)
-        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.1)
+        scheduler = optim.lr_scheduler.StepLR(
+            optimizer, step_size=1, gamma=0.1)
         return [optimizer], [scheduler]
-
-    def forward(self, source_ids, source_mask, labels):
-        return self.pretrained_model(source_ids,
-                                     attention_mask=source_mask,
-                                     labels=labels)
 
     def training_step(self, batch, batch_idx):
         source, target = batch
         labels = target['input_ids'].clone()
         labels[labels == self.tokenizer.pad_token_id] = -100
-        outputs = self.forward(source['input_ids'],
-                        source['attention_mask'],
-                        labels=labels)
+        outputs = self.pretrained_model(input_ids=source['input_ids'],
+                                        attention_mask=source['attention_mask'],
+                                        labels=labels)
         loss = outputs[0]
         self.log('train_loss', loss)
         return loss
@@ -46,9 +52,9 @@ class Code2TestModel(pl.LightningModule):
         source, target = batch
         labels = target['input_ids'].clone()
         labels[labels == self.tokenizer.pad_token_id] = -100
-        outputs = self.forward(source['input_ids'],
-                        source['attention_mask'],
-                        labels=labels)
+        outputs = self.pretrained_model(input_ids=source['input_ids'],
+                                        attention_mask=source['attention_mask'],
+                                        labels=labels)
         loss = outputs[0]
         self.log('val_loss', loss)
         return loss
